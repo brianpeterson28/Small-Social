aaa624a069f94b59dee717227df8dbae09e28ecf
Brian Peterson <brianpeterson28@gmail.com>
2018-11-12 22:03:23 -0600

    small social
    Created initial db model, user model, forms, and html templates for
    basic social blogging app

---
+++ forms.py
@@ -0,0 +1,43 @@
+#from flask_wtf import Form
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import (DataRequired, Regexp, ValidationError, Email,
+                                Length, EqualTo)
+
+from models import User
+
+rf_message = ("Username should be one word, letters, numbers, and underscores "
+              "only.")
+pw_message = ("Passwords must match.")
+
+def name_exists(form, field):
+    if User.select().where(User.username == field.data).exists():
+        raise ValidationError('User with that name already exists.')
+
+def email_exists(form, field):
+    if User.select().where(User.email == field.data).exists():
+        raise ValidationError('User with that e-mail already exists.')
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username',
+                            validators=[DataRequired(),
+                                        Regexp(r'^[a-zA-Z0-9_]+$', 
+                                        message=rf_message),
+                                        name_exists]
+                            )
+
+    email = StringField('Email',
+                        validators=[DataRequired(),
+                                    Email(),
+                                    email_exists
+                                    ])
+
+    password = PasswordField('Password',
+                              validators=[DataRequired(),
+                                          Length(min=2),
+                                          EqualTo('password2', 
+                                                   message=pw_message)
+                                          ])
+
+    password2 = PasswordField('Confirm Password',
+                               validators=[DataRequired()])

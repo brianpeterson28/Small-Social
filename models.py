5f83916437dd31cea5e404b49d4cfbd60a465ca3
Brian Peterson <brianpeterson28@gmail.com>
2019-01-21 17:46:26 -0600

    Post Table
    Update initialize method so that table to hold posts will be created.

--- models.py
+++ models.py
@@ -47,5 +47,5 @@ class Post(Model):
 
 def initialize():
     DATABASE.connect()
-    DATABASE.create_tables([User], safe=True)
+    DATABASE.create_tables([User, Post], safe=True)
     DATABASE.close()
